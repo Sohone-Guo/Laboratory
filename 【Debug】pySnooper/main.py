4ac2d264213b8c1111bbe3a2fd5097d82b4eec2b
Sohone <>
2019-04-24 10:04:34 +0800

    pysnooper

---
+++ 【Debug】pySnooper/main.py
@@ -0,0 +1,21 @@
+import pysnooper
+a = 10
+
+@pysnooper.snoop()
+def count(value):
+    total = []
+    a = len(total)
+    for item in range(value):
+        total.append(item)
+    a = len(total)
+    return total
+
+def test():
+    a = 1
+    return a
+
+
+
+if __name__ == "__main__":
+    a = count(5)
+    a = test()
